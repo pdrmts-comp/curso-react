528dc1e2d2bbf162bc233b012ed3d116f228abd1
pedro <rgm52664@comp.uems.br>
2025-09-04 14:54:33 -0400

    paginas home estilizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from 'react'
 import { Link } from 'react-router';
+import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>TESTE</h1>
-      <ul>
-        <li><Link to="/cadastro">CADASTRO</Link></li>
-        <li><Link to="/lista">LISTA</Link></li>
-      </ul>
+      <div className='flex flex-col justify-center items-center min-h-screen'>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <h1 className="text-4xl font-extrabold mb-4">
+          BEM VINDO AO MEU PROJETO REACT
+        </h1>
+        <p className='mb-8'>
+          GERENCIE USUÁRIOS COM CADASTRO E LISTAGEM 
+        </p>
+       
+        <ul className='flex gap-5'>
+          <li><Link to="/cadastro">CADASTRO</Link></li>
+          <li><Link to="/lista">LISTA</Link></li>
+        </ul>
+      </div>
     </>
   )
 }
